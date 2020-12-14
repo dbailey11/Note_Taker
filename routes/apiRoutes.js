4778b286b4a1823587f123f59d2f4aa9a9255c9e
Devyn Bailey <devynbailey11@yahoo.com>
2020-12-13 19:39:47 -0700

    fixed html/css links, added more code into route js files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,31 @@
+const fs = require('fs');
 const path = require('path');
 const noteData = require('../db/db.json');
 
 module.exports = (app) => {
 
+
     //GET to return notes
-    app.get('/api/notes', (req, res) => res.json(noteData));
+    app.get('/api/notes', (req, res) => {
+        fs.readFile(path.join(__dirname, '/db/db.json'), "utf8", (err, data) => {
+            if(err) throw err;
+            res.json(noteData);
+        });
+    });
 
     //POST adds notes
     app.post('/api/notes', (req, res) => {
-
+        let newNote = req.body;
+        let id = noteData.length;
+        newNote.id = id + 1;
+        noteData.push(newNote);
+        return res.json(noteData);
+       
     });
 
     //Delete posts
     app.delete('/api/notes/:id', (req, res) => {
-        
+
     })
 
 }
