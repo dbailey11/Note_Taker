c1ffbe22ea6f85af48d5e108a0e6ab1d02d13d8b
Devyn Bailey <devynbailey11@yahoo.com>
2020-12-15 17:59:38 -0700

    cleaned up apiRoute.js code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,19 +13,10 @@ module.exports = (app) => {
     let id = dbNotes.length;
     note.id = id + 1;
     dbNotes.push(note);
-    dbUpdate(dbNotes);
     return res.json(dbNotes);
   });
 
-
-  // Delete posts
-  app.delete("/api/notes/:id", (req, res) => {
-    let id = req.params.id;
-    let x = 1;
-    delete dbNotes[id - 1];
-    dbUpdate(dbNotes);
-    res.send(dbNotes);
-  });
+  // app.delete
 
 };
 
